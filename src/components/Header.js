037269904ceefca41ecfd8392d868b1936df95ea
Simone823 <simone.daglio823@outlook.it>
2022-10-21 16:23:25 +0200

    header

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Header() {
+  return (
+    // header
+    <header>
+        <div className='container'>
+            <nav className="navbar">
+                <div className="container-fluid">
+                    <span className="navbar-brand mb-0 h1 fs-4 fw-bold">Nice Cream</span>
+                </div>
+            </nav>
+        </div>
+    </header>
+  )
+}
+
+export default Header
