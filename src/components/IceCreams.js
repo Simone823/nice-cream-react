1b7c73c423ea9cf0882d7e01b6904fdc1c8177d8
Simone823 <simone.daglio823@outlook.it>
2022-10-22 11:05:37 +0200

    ice creams completo

--- src/components/IceCreams.js
+++ src/components/IceCreams.js
@@ -108,32 +108,53 @@ function IceCreams() {
                     isLoading === false && isError === false ? (
                         // row
                         <div className='row'>
-                            <div className='col-12'>
-                                {/* list categories */}
-                                <ul className='list-categories d-flex align-items-center justify-content-evenly flex-wrap gap-3 mb-4'>
-                                    {/* map categories */}
-                                    {categories.map((category, index) => {
-                                        return (
-                                            <li key={index}>
-                                                <button onClick={filterProducts(category, index)} type='button' className={`px-3 py-2 text-uppercase fw-bolder ${activeCategory === index ? 'active' : ''}`}>{category}</button>
-                                            </li>
-                                        )
-                                    })}
-                                </ul>
+                            {/* list categories */}
+                            <ul className='list-categories d-flex align-items-center justify-content-evenly flex-wrap gap-3 mb-4'>
+                                {/* map categories */}
+                                {categories.map((category, index) => {
+                                    return (
+                                        <li key={index}>
+                                            <button onClick={filterProducts(category, index)} type='button' className={`px-3 py-2 text-uppercase fw-bolder text-black ${activeCategory === index ? 'active' : ''}`}>{category}</button>
+                                        </li>
+                                    )
+                                })}
+                            </ul>
 
-                                {/* list filtered products */}
-                                <ul className='filtered-products d-flex flex-wrap align-items-center'>
-                                    {/* map filtered products */}
-                                    {filteredProducts.map((product) => {
-                                        return(
-                                            <li key={product.id} className="col-12 col-md-6">
-                                                {product.nome}
-                                            </li>
-                                        )
-                                    })}
+                            {/* list filtered products */}
+                            <ul className='filtered-products d-flex flex-wrap align-items-center gx-md-3'>
+                                {/* map filtered products */}
+                                {filteredProducts.map((product) => {
+                                    return(
+                                        <li key={product.id} className="col-12 col-md-6 px-4">
+                                            {/* card */}
+                                            <div className='card py-3 px-3 border-0 d-flex flex-row flex-wrap'>
+                                                {/* figure */}
+                                                <figure className='product-image col-12 col-md-6'>
+                                                    <img src={product.img}  alt={product.nome} />
+                                                </figure>
 
-                                </ul>
-                            </div>
+                                                {/* description */}
+                                                <div className='description col-12 col-md-6 flex-grow-1'>
+                                                    {/* text heade */}
+                                                    <div className='text-head'>
+                                                        <h5 className='mb-0 fw-bolder d-flex justify-content-between'>
+                                                            {product.nome} 
+                                                            <span className='fw-bolder text-white'>{(product.prezzo / 100).toFixed(2)} &euro;</span>
+                                                        </h5>
+                                                        <h6 className='mb-0 fw-bolder'>{product.categoria}</h6>
+                                                    </div>
+
+                                                    {/* text bottom */}
+                                                    <div className='text-bottom'>
+                                                        <p className='mb-0'>{product.descrizione}</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    )
+                                })}
+
+                            </ul>
                         </div>
                     ) : // Se is loading è false ma is error è true mostro titolo errore
                     isLoading === false && isError === true ? (
